533ac9a8b1b5b8ee3a2d3f6a11c3cd38872aa326
shihab-newaz <shihab.newaz.5633@gmail.com>
2024-10-08 02:03:55 +0600

    changes for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // src/app/page.tsx
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import HomePage from './home/page';
 export default function Home() {
   return (
